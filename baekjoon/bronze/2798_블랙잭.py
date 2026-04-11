b73effd60776cdb54f7b6d59e6ec9317d77789ec
surt321 <surt321@khu.ac.kr>
2026-04-11 23:44:55 +0900

    feat: 2798_블랙잭

---
+++ baekjoon/bronze/2798_블랙잭.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+answer = 0
+
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            sum = cards[i] + cards[j] + cards[k]
+            if sum <= M:
+                answer = max(sum, answer)
+
+print(answer)
---
+++ baekjoon/bronze/9086_문자열.py
@@ -0,0 +1,5 @@
+T = int(input())
+
+for i in range(T):
+    INPUT_STR = input()
+    print(INPUT_STR[0] + INPUT_STR[-1])
